bd1fc7e0e9535ab74ce5f70e82a3808f1e761eea
xxsamiraf <samiradg105@gmail.com>
2022-05-20 14:16:22 -0400

    update

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
 const searchForm = document.querySelector('form')
-const searchResultDiv = document.querySelector('.search-result')
+const searchResultDivDes = document.querySelector('.des')
+const searchResultDiv = document.querySelector('.wine-search-box')
 const container = document.querySelector('.container')
 let searchQuery = '';
-const APP_ID = '9a2e63f7';
-const APP_key = '1c01fc5b9e81959107b882b7ec3525e9';
+const APP_key = 'd6c482d892e8481d85c13aa1ba0328cd';
 
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     searchQuery = e.target.querySelector('input').value;
+    fetchAPI();
 });
 
-async
+async function fetchAPI(){
+    const baseURL = `https://api.spoonacular.com/food/wine/pairing?apiKey=${APP_key}&food=${searchQuery}`;
+    const response = await fetch(baseURL);
+    const data = await response.json();
+    generateHTML(data);
+    console.log(data);
+}
+
+function generateHTML(results){
+    container.classList.remove('initial');
+    let generatedHTML0 = `<p class="description">${results.pairingText}</p>`;
+    let generatedHTML = '';
+    results.productMatches.map(result => {
+        generatedHTML +=
+        `
+        <div class="item">
+            <img src=${result.imageUrl} alt="">
+            <div class="flex-container">
+                <h1 class="title">${result.title}</h1>
+                <a class='view-button' href="${result.link}">Purchase</a>
+            </div>
+            <p class="item-data">Price: ${result.price}</p>
+            <p class="item-data">Rating: ${result.ratingCount}</p>
+            <p class="item-data">Score: ${result.score.toFixed(2)}</p>
+        </div>
+        `
+    })
+    searchResultDivDes.innerHTML = generatedHTML0;
+    searchResultDiv.innerHTML = generatedHTML;
+}
